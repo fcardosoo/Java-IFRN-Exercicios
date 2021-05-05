f18a20390c2c043b1e21113c3c4752a0f3c56160
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2021-05-05 00:15:47 -0300

    | IFRN | Exercícios

---
+++ Aula12-ex03/Aula12-ex03-Pessoa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula12;
+
+/**
+ *
+ * @author Fabiano
+ */
+public class Aula12_ex03_Pessoa {
+    public String nome;
+    public int idade;
+    public float peso;
+    public Aula12_ex03_Sexo sexo;
+}

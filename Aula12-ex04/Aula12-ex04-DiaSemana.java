f4e0e111ec8a8f7c37ccb2f5ce756c888b5faa9f
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2021-05-12 13:08:36 -0300

    | IFRN | Exercício

---
+++ Aula12-ex04/Aula12-ex04-DiaSemana.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula12;
+
+/**
+ *
+ * @author Fabiano
+ */
+public enum Aula12_ex04_DiaSemana {
+    DOMINGO, SEGUNDA_FEIRA,TERCA_FEIRA, QUARTA_FEIRA,
+    QUINTA_FEIRA, SEXTA_FEIRA, SABADO
+}

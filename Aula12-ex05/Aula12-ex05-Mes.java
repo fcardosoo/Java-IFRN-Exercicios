335c41007bf131a7caa235b47223c35f6d765226
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2021-05-12 13:21:50 -0300

    | IFRN | Exercícios

---
+++ Aula12-ex05/Aula12-ex05-Mes.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula12;
+
+/**
+ *
+ * @author Fabiano
+ */
+public enum Aula12_ex05_Mes {
+    JANEIRO, FEVEREIRO, MARCO, ABRIL, MAIO, JUNHO,
+    JULHO, AGOSTO, SETEMBRO, OUTUBRO, NOVEMBRO, DEZEMBRO
+}
